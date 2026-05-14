3f91e07d07ddcb50cc02c9aba2fdc420ef16e445
Abdelrahman Mostafa <abdomostafamohamed1234@gmail.com>
2026-05-14 20:41:50 +0300

    feat: Product Details Screen

--- lib/config/routing/routing_generator.dart
+++ lib/config/routing/routing_generator.dart
@@ -1,12 +1,30 @@
+import 'package:flowery/config/routing/app_routes.dart';
+import 'package:flowery/features/products_details/presentation/pages/products_details_screen.dart';
 import 'package:flutter/material.dart';
+
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
     try {
       switch (settings.name) {
-        // case AppRoutes.login:
-        //   return MaterialPageRoute(
-        //     builder: (_) => const LoginScreen(),
-        //   );
+        case AppRoutes.productDetails:
+          // final args = settings.arguments as Map<String, dynamic>;
+          return MaterialPageRoute(
+            builder: (_) => const ProductsDetailsScreen(
+              imageUrl: '',
+              title: ' Pink Rose ',
+              price: 1500,
+              isdescount: false,
+              oldPrice: 0.0,
+              discount: 0.0,
+              sold: 0,
+              quantity: 15,
+              images: [],
+              //               images: List<String>.from(
+              //   args[CategoriesValues.images],
+
+              // ),
+            ),
+          );
 
         default:
           return unDefinedRoute();
@@ -19,12 +37,8 @@ class RouteGenerator {
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
       builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: const Text('No Route Found'),
-        ),
-        body: const Center(
-          child: Text('No Route Found'),
-        ),
+        appBar: AppBar(title: const Text('No Route Found')),
+        body: const Center(child: Text('No Route Found')),
       ),
     );
   }
@@ -32,12 +46,8 @@ class RouteGenerator {
   static Route<dynamic> errorRoute(String error) {
     return MaterialPageRoute(
       builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Route Error'),
-        ),
-        body: Center(
-          child: Text(error),
-        ),
+        appBar: AppBar(title: const Text('Route Error')),
+        body: Center(child: Text(error)),
       ),
     );
   }
--- lib/features/products_details/presentation/pages/products_details_screen.dart
+++ lib/features/products_details/presentation/pages/products_details_screen.dart
@@ -1,18 +1,194 @@
-import 'package:flutter/widgets.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProductsDetailsScreen extends StatefulWidget {
   final String imageUrl;
   final String title;
-  
+  final double price;
+  final bool isdescount;
+  final double oldPrice;
+  final double discount;
+  final int sold;
+  final int quantity;
+  final List<String> images;
 
-  const ProductsDetailsScreen({super.key, required this.imageUrl, required this.title});
+  const ProductsDetailsScreen({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+    required this.isdescount,
+    required this.oldPrice,
+    required this.discount,
+    required this.sold,
+    required this.quantity,
+    required this.images,
+  });
   @override
   State<ProductsDetailsScreen> createState() => _ProductsDetailsScreenState();
 }
 
 class _ProductsDetailsScreenState extends State<ProductsDetailsScreen> {
+  int _currentIndex = 0;
+  final CarouselSliderController _controller = CarouselSliderController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                CarouselSlider.builder(
+                  carouselController: _controller,
+                  itemCount: widget.images.length,
+                  itemBuilder: (context, index, realIndex) {
+                    return Image.asset(widget.images[index], fit: BoxFit.cover);
+                  },
+                  options: CarouselOptions(
+                    height: 430.h,
+                    viewportFraction: 1.0,
+                    enableInfiniteScroll: false,
+                    onPageChanged: (index, reason) =>
+                        setState(() => _currentIndex = index),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  bottom: 16.h,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: List.generate(
+                      widget.images.length,
+                      (index) => GestureDetector(
+                        onTap: () => _controller.animateToPage(index),
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          width: _currentIndex == index ? 12 : 8,
+                          height: _currentIndex == index ? 12 : 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: _currentIndex == index
+                                ? Color(
+                                    Theme.of(context).colorScheme.primary.value,
+                                  )
+                                : Color(
+                                    Theme.of(
+                                      context,
+                                    ).colorScheme.onSecondary.value,
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'EGY ${widget.price}',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontWeight: .w700,
+                          fontSize: 20.sp,
+                        ),
+                      ),
+                      Text(
+                        'Status:in stock',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontWeight: .w500,
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'All prices include tax',
+                    style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      fontSize: 13.sp,
+                      fontWeight: .w400,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                  Text(
+                    '${widget.quantity} ${widget.title}Bouquet',
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontSize: 16.sp,
+                      fontWeight: .w500,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                  SizedBox(height: 24.h),
+                  Text(
+                    'Description:',
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontSize: 16.sp,
+                      fontWeight: .w500,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                  SizedBox(height: 60.h),
+                  Text(
+                    'Bouquet include:',
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontSize: 16.sp,
+                      fontWeight: .w500,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Text(
+                    '${widget.title}:${widget.quantity}',
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontSize: 16.sp,
+                      fontWeight: .w500,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 50),
+                ),
+                onPressed: () {
+                  // Handle button press
+                },
+                child: const Text('Add to Cart'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
