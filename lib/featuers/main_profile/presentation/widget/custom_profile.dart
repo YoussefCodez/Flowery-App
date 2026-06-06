34c96286ca847526099627fb17ee9ea42008e99e
tadroussamirhanna-rgb <tadroussamirhanna@gmail.com>
2026-06-06 19:37:17 +0300

    feat : search debounce and unit test

---
+++ lib/featuers/main_profile/presentation/widget/custom_profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomProfile extends StatelessWidget {
+  final Widget? leadingIcon;
+  final String? title;
+  final Widget? trailingIcon;
+
+
+  const CustomProfile({super.key,this.leadingIcon,required this.title,this.trailingIcon,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [],);
+  }
+}
