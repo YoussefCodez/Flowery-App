f34f23d07ebece8e1fa60db0723de08152b90b68
abdelrahman <abdelrahmanobo12@gmail.com>
2026-06-06 23:42:00 +0300

    feat(change-password): streamline route definitions and clean up commented code

--- lib/config/routing/routing_generator.dart
+++ lib/config/routing/routing_generator.dart
@@ -1,12 +1,15 @@
+import 'package:flowery/config/routing/app_routes.dart';
+import 'package:flowery/features/change_password/presentation/screens/change_password_screen.dart';
 import 'package:flutter/material.dart';
+
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
     try {
       switch (settings.name) {
-        // case AppRoutes.login:
-        //   return MaterialPageRoute(
-        //     builder: (_) => const LoginScreen(),
-        //   );
+        case AppRoutes.changePassword:
+          return MaterialPageRoute(
+            builder: (_) => const ChangePasswordScreen(),
+          );
 
         default:
           return unDefinedRoute();
@@ -19,12 +22,8 @@ class RouteGenerator {
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
       builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: const Text('No Route Found'),
-        ),
-        body: const Center(
-          child: Text('No Route Found'),
-        ),
+        appBar: AppBar(title: const Text('No Route Found')),
+        body: const Center(child: Text('No Route Found')),
       ),
     );
   }
@@ -32,12 +31,8 @@ class RouteGenerator {
   static Route<dynamic> errorRoute(String error) {
     return MaterialPageRoute(
       builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Route Error'),
-        ),
-        body: Center(
-          child: Text(error),
-        ),
+        appBar: AppBar(title: const Text('Route Error')),
+        body: Center(child: Text(error)),
       ),
     );
   }
