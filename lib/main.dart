6a55342c83aded3fbfb845af7e5c3bf224496df5
abdelrahman <abdelrahmanobo12@gmail.com>
2026-06-10 01:31:52 +0300

    refactor: Remove unused import of app_routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'config/routing/app_routes.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
