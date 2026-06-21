3951dce857590551e406331733436a88f12157cb
abdelrahman <abdelrahmanobo12@gmail.com>
2026-06-21 15:12:57 +0300

    feat: Implement filtering and sorting functionality with localization support

---
+++ lib/config/l10n/translations/app_localizations_ar.dart
@@ -0,0 +1,368 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get remember_me => 'تذكرني';
+
+  @override
+  String get sign_up => 'انشاء حساب';
+
+  @override
+  String get enter_your_email => 'ادخل البريد الإلكتروني';
+
+  @override
+  String get enter_your_password => 'ادخل كلمة المرور';
+
+  @override
+  String get name_is_required => 'الاسم مطلوب!';
+
+  @override
+  String get name_is_not_valid => 'الاسم غير صحيح';
+
+  @override
+  String get email_is_required => 'البريد الإلكتروني مطلوب!';
+
+  @override
+  String get email_is_not_valid => 'البريد الإلكتروني غير صحيح';
+
+  @override
+  String get password_is_required => 'كلمة المرور مطلوبة!';
+
+  @override
+  String get password_is_not_valid => 'كلمة المرور غير صحيحة';
+
+  @override
+  String get password_must_be_at_least_6_characters =>
+      'كلمة المرور يجب أن تكون 6 أحرف على الأقل';
+
+  @override
+  String get passwords_do_not_match => 'كلمتا المرور غير متطابقتين';
+
+  @override
+  String get confirm_password_is_required => 'تأكيد كلمة المرور مطلوب!';
+
+  @override
+  String get confirm_password_is_not_valid => 'تأكيد كلمة المرور غير صحيح';
+
+  @override
+  String get password_and_confirm_password_must_be_same =>
+      'يجب أن تكون كلمتا المرور متطابقتين';
+
+  @override
+  String get phone_number_is_required => 'رقم الهاتف مطلوب!';
+
+  @override
+  String get no_internet => 'لا يوجد اتصال بالإنترنت!';
+
+  @override
+  String get connectionError => 'فشل الاتصال بالخادم';
+
+  @override
+  String get connectionTimeout => 'فشل الاتصال بالخادم';
+
+  @override
+  String get cancelled => 'تم إلغاء الطلب إلى الخادم';
+
+  @override
+  String get unknown =>
+      'حدث خطأ غير متوقع في الاتصال بالخادم ، يرجى المحاولة لاحقًا!';
+
+  @override
+  String get server_error => 'خطأ في الخادم، يرجى المحاولة لاحقًا!';
+
+  @override
+  String get receiveTimeout => 'فشل الاتصال بالخادم أثناء استلام البيانات';
+
+  @override
+  String get sendTimeout => 'فشل الاتصال بالخادم أثناء إرسال البيانات';
+
+  @override
+  String get unexpected_error => 'حدث خطأ غير متوقع';
+
+  @override
+  String get badCertificate => 'شهادة غير صالحة من الخادم';
+
+  @override
+  String get expiredToken => 'انتهت صلاحية الجلسة، يرجى تسجيل الدخول مرة أخرى';
+
+  @override
+  String get phone_number_is_not_valid => 'رقم الهاتف غير صحيح';
+
+  @override
+  String get this_field_is_required => 'هذا الحقل مطلوب';
+
+  @override
+  String get error => 'خطأ';
+
+  @override
+  String get ok => 'حسناً';
+
+  @override
+  String get hey_there => 'مرحباً';
+
+  @override
+  String get welcome_back => 'مرحباً بعودتك';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forget_password_ques => 'نسيت كلمة المرور؟';
+
+  @override
+  String get register => 'إنشاء حساب';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get settings => 'الإعدادات';
+
+  @override
+  String get help => 'مساعدة';
+
+  @override
+  String get dark_mode => 'الوضع الداكن';
+
+  @override
+  String get light_mode => 'الوضع الفاتح';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get categories => 'الفئات';
+
+  @override
+  String get cart => 'السلة';
+
+  @override
+  String get profile => 'الملف الشخصي';
+
+  @override
+  String get hi => 'مرحباً';
+
+  @override
+  String get apply => 'تقديم';
+
+  @override
+  String get what_is_new => 'ما الجديد؟';
+
+  @override
+  String get sea_all => 'عرض الكل';
+
+  @override
+  String get enter_your_username_and_password_to_login =>
+      'أدخل اسم المستخدم وكلمة المرور لتسجيل الدخول';
+
+  @override
+  String get username => 'اسم المستخدم';
+
+  @override
+  String get or_login_with => 'أو سجل باستخدام';
+
+  @override
+  String get don_t_have_an_account => 'ليس لديك حساب؟';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get confirm_password => 'تأكيد كلمة المرور';
+
+  @override
+  String get mobile_number => 'رقم الهاتف';
+
+  @override
+  String get register_as => 'التسجيل كـ';
+
+  @override
+  String get google => 'جوجل';
+
+  @override
+  String get facebook => 'فيسبوك';
+
+  @override
+  String get select_an_option => 'اختر خياراً';
+
+  @override
+  String get login_success => 'تم تسجيل الدخول بنجاح';
+
+  @override
+  String get register_success => 'تم التسجيل بنجاح';
+
+  @override
+  String get connectButton => 'ربط';
+
+  @override
+  String get an_error_occurred => 'حدث خطأ';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get cancel => 'إلغاء';
+
+  @override
+  String get confirm_logout => 'تأكيد تسجيل الخروج!!';
+
+  @override
+  String get change_language => 'تغيير اللغة';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get enterFirstName => 'ادخل الاسم الاول';
+
+  @override
+  String get firstName => 'الاسم الاول';
+
+  @override
+  String get enterLastName => 'ادخل الاسم الاخير';
+
+  @override
+  String get lastName => 'الاسم الاخير';
+
+  @override
+  String get enterYourEmail => 'ادخل ايميلك';
+
+  @override
+  String get enterPassword => 'ادخل الرقم السري';
+
+  @override
+  String get enterPhoneNumber => 'ادخل رقم الهاتف';
+
+  @override
+  String get phoneNumber => 'رقم الهاتف';
+
+  @override
+  String get gender => 'الجنس';
+
+  @override
+  String get female => 'انثي';
+
+  @override
+  String get male => 'رجل';
+
+  @override
+  String get creatingAccountAgreement => 'بإنشاء حساب، فإنك توافق على شروطنا';
+
+  @override
+  String get termsAndConditions => 'الشروط والأحكام';
+
+  @override
+  String get egp => 'جنيه';
+
+  @override
+  String get status_in_stock => 'الحالة: متوفر في المخزون';
+
+  @override
+  String get all_prices_include_tax => 'جميع الأسعار تشمل الضريبة';
+
+  @override
+  String get bouquet => 'Bouquet';
+
+  @override
+  String get description => 'الوصف';
+
+  @override
+  String get bouquet_include => 'تشمل الباقة';
+
+  @override
+  String get add_to_cart => 'اضف الى العربة';
+
+  @override
+  String get reset_password => 'إعادة تعيين كلمة المرور';
+
+  @override
+  String get current_password => 'كلمة المرور الحالية';
+
+  @override
+  String get new_password => 'كلمة المرور الجديدة';
+
+  @override
+  String get update => 'تحديث';
+
+  @override
+  String get incorrect_password => 'كلمة المرور القديمة غير صحيحة';
+
+  @override
+  String get password_changed_success => 'تم تغيير كلمة المرور بنجاح';
+
+  @override
+  String get success => 'نجحت';
+
+  @override
+  String get best_seller => 'الأكثر مبيعًا';
+
+  @override
+  String get best_seller_title => 'تألقي مع أفضل منتجاتنا مبيعًا';
+
+  @override
+  String get no_products => 'لا يوجد منتجات';
+
+  @override
+  String get occasion => 'المناسبة';
+
+  @override
+  String get lowest_price => 'ارخص ثمن';
+
+  @override
+  String get highest_price => 'اغلي ثمن';
+
+  @override
+  String get new_products => 'جديد';
+
+  @override
+  String get old_products => 'قديم';
+
+  @override
+  String get filter => 'فلتر';
+
+  @override
+  String get sort_by => 'فرز حسب';
+
+  @override
+  String get items => 'العناصر';
+
+  @override
+  String get sub_total => 'المجموع الفرعي';
+
+  @override
+  String get delivery_fee => 'رسوم التوصيل';
+
+  @override
+  String get total => 'الإجمالي';
+
+  @override
+  String get checkout => 'إتمام الطلب';
+
+  @override
+  String get discount => 'خصم';
+
+  @override
+  String get sub_total_after_discount => 'المجموع الفرعي بعد الخصم';
+
+  @override
+  String get your_cart_is_empty => 'سلتك فارغة';
+}
